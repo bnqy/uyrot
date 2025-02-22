16c491dc014ce5c9a65dc86acd8672cad6c10f8d
bnqy <obbonobo@gmail.com>
2025-02-23 04:15:13 +0600

    Header added.

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg'
 import profile_pic from './profile_pic.jpg'
 import dropdown_icon from './dropdown_icon.svg'
-
+import arrow_icon from './arrow_icon.svg'
+import group_profiles from './group_profiles.png'
+import header_img from './header_img.png'
 
 
 export const assets = {
    logo,
    profile_pic,
-   dropdown_icon
+   dropdown_icon,
+   arrow_icon,
+   group_profiles,
+   header_img
 }
