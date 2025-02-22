f6d38d642f3fcc7142e3c28610e35c42ceb014c2
bnqy <obbonobo@gmail.com>
2025-02-23 03:01:31 +0600

    Navbar implemented.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Home from './pages/Home'
+import Tutors from './pages/Tutors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/tutors' element={<Tutors />}/>
+        <Route path='/tutors:department' element={<Tutors />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/my-profile' element={<MyProfile />}/>
+        <Route path='/my-appointments' element={<MyAppointments />}/>
+        <Route path='/appointment/:tutId' element={<Appointment />}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg'
+import profile_pic from './profile_pic.jpg'
+import dropdown_icon from './dropdown_icon.svg'
+
+
+
+export const assets = {
+   logo,
+   profile_pic,
+   dropdown_icon
+}
