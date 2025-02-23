d53177fc22b7b486ca1738ea881ad0dea37a1af6
bnqy <obbonobo@gmail.com>
2025-02-23 15:38:56 +0600

    Toptutors section.

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+import { tutors } from "../assets/assets";
+
+export const AppContext = createContext();
+
+const AppContextProvider = (props) =>{
+
+    const currencySymbol = 'c.';
+
+    const value = {
+        tutors,
+        currencySymbol
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+export default AppContextProvider
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import DepartmentsMenu from '../components/DepartmentsMenu'
+import TopTutors from '../components/TopTutors'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <DepartmentsMenu />
+      <TopTutors />
     </div>
   )
 }
